3fc7b203c403e81954f3f9984399d486c90bab38
Rus <punyarit07@gmail.com>
2023-03-18 05:50:58 +0700

    add buttonPress game

--- basic/main.lua
+++ basic/main.lua
@@ -1,54 +1,7 @@
--- variable
--- global
-message = 0
-word = "Rus"
-chicken = 10
-output = chicken + 1
--- local
-local var = "I'm local variable"
-
-
--- while loop
-count = 0
-while count < 100 do
-    count = count + 1
-end 
-
-
--- for loop
-for count=0, 7, 1 do
-    chicken = 7
-end
-
-
--- function
-function increaseMessage(i)
-    message = message + i
-end
-
--- return function
-function double(val)
-    val = val * 2
-    return val
-end
-
--- function called
-increaseMessage(21)
-increaseMessage(chicken)
-double(3.5)
-double(chicken)
-
-
--- comment
--- one line comment
---[[ 
-    mutiple line comment
-]]
-
+message = "String text"
+number = 5
 
 function love.draw()
-    -- newFont(font-size)
     love.graphics.setFont(love.graphics.newFont(50))
-    -- love.graphics.print(message) -- message value
-    -- love.graphics.print(double(3.5)) -- call function and return to print
+    love.graphics.print(message)
 end
---
+++ buttonPress/main.lua
@@ -0,0 +1,47 @@
+function love.load()
+    button = {}
+    button.x = 200
+    button.y = 200
+    button.size = 50
+
+    score = 0
+    timer = 0
+
+    myFont = love.graphics.newFont(40)
+end
+
+
+function love.update(dt)
+
+end
+
+
+function love.draw()
+    love.graphics.setFont(myFont)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print(score)
+
+    love.graphics.setColor(255/255, 70/255, 70/255)
+    love.graphics.circle("fill", button.x, button.y, button.size)
+end
+
+
+-- call this function when mouse is click
+function love.mousepressed(x, y, b, istouch)
+    -- 1: primary, 2: secondary, 3: middle
+    if b == 1 then
+        if distanceBetween(
+            button.x, button.y, 
+            love.mouse.getX(), love.mouse.getY()) < button.size 
+        then
+            score = score + 1
+            button.x = math.random(button.size, love.graphics.getWidth() - button.size)
+            button.y = math.random(button.size, love.graphics.getHeight() - button.size)
+        end
+    end
+end
+
+
+function distanceBetween(x1, y1, x2, y2)
+    return math.sqrt(((y2 - y1)^2) + ((x2 - x1)^2))
+end
