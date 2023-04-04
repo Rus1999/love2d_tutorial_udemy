90b47b55e0456b2384e7f3558d1aa3e11c01328a
Rus <punyarit07@gmail.com>
2023-04-04 15:43:48 +0700

    finsihing up

--- ShootingGallery/main.lua
+++ ShootingGallery/main.lua
@@ -5,29 +5,51 @@ function love.load()
   target.radius = 50
 
   score = 0
-  timer = 2 -- countdown
+  timer = 0 -- countdown
+  gameState = 1 -- 1: main menu, 2: gameplay
 
   gameFont = love.graphics.newFont(40)
+
+  -- set sprites
+  sprites = {}
+  sprites.sky = love.graphics.newImage('sprites/sky.png')
+  sprites.target = love.graphics.newImage('sprites/target.png')
+  sprites.crosshairs = love.graphics.newImage('sprites/crosshairs.png')
+
+  -- game settings
+  love.mouse.setVisible(false)
 end
 
 function love.update(dt) -- 60 fps
-  if (timer > 0) then
+  if timer > 0 then
     timer = timer - dt
   end
 
-  if (timer < 0) then
+  if timer < 0 then
     timer = 0
+    gameState = 1
   end
 end
 
 function love.draw() -- 60 fps graphical related only
-  love.graphics.setColor(255/255, 77/255, 77/255)
-  love.graphics.circle("fill", target.x, target.y, target.radius)
+  love.graphics.draw(sprites.sky, 0, 0)
   
   love.graphics.setColor(1, 1, 1)
   love.graphics.setFont(gameFont)
-  love.graphics.print(score, 0, 0)
-  love.graphics.print(math.ceil(timer), 100, 0)
+  love.graphics.print("Score: " .. score, 0, 5)
+  love.graphics.print("Timer: " .. math.ceil(timer), love.graphics.getWidth() - 200, 5)
+
+  if gameState == 1 then
+    love.graphics.printf("Click anywhere to play!", 
+                          0, 250, love.graphics.getWidth(), "center")
+  end
+
+  if gameState == 2 then
+    love.graphics.draw(sprites.target, 
+                        target.x - target.radius, target.y - target.radius)
+  end
+  love.graphics.draw(sprites.crosshairs, 
+                      love.mouse.getX()-20, love.mouse.getY()-20)
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
@@ -35,17 +57,21 @@ function love.mousepressed(x, y, button, istouch, presses)
     x, y : mouse position when mouse is pressesd
     button : which button is pressed (1:primary, 2:secondary, 3:middle)
   ]]
-  if (button == 1) then
+  if button == 1 and gameState == 2 then
     local distanceBetweenMouseToTarget = 
                 distanceBetween(x, y, target.x, target.y)
-    if (distanceBetweenMouseToTarget < target.radius) then
+    if distanceBetweenMouseToTarget < target.radius then
       -- mouse is over the target
       score = score + 1
       target.x = math.random(target.radius, 
                               love.graphics.getWidth() - target.radius)
-      target.y = math.random(target.radius, 
+      target.y = math.random(target.radius + 50, 
                               love.graphics.getHeight() - target.radius)
     end
+  elseif button == 1 and gameState == 1 then
+    gameState = 2
+    timer = 28
+    score = 0
   end
 end
 
